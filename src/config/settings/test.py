06a161d4bbdca02eb6df5ce141adc7bfb8e46846
faouzi_ben <mohamed.faouzibensalah@go-aos.io>
2022-01-31 01:24:53 +0100

    Add test_models for prog_immo

--- src/config/settings/test.py
+++ src/config/settings/test.py
@@ -1,24 +1,12 @@
 """
+Django base settings for social network project.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
-
-For more information on production settings
-https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 """
 from .base import *
 from os import getenv
-
-# ### DEBUG CONFIGURATION ###########################
-DEBUG = True
-
-# ### HOST CONFIGURATION ##############################
-ALLOWED_HOSTS = getenv('ALLOWED_HOSTS', '0.0.0.0,').split(',')
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
--- src/config/urls.py
+++ src/config/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     # API URLS
     path('api/', include(('apps.urls', 'apps'), namespace='api')),
 
+    # swagger urls
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
